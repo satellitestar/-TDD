bdf87e61809b43eabcdbbcfbd0cfa07abec648f7
satellitestar <1752553357@qq.com>
2020-05-31 01:07:39 +0800

    第四个ppt昨晚，找不到tr标签没有解决

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -32,9 +33,9 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New To-Do item did not appear in table"
         )
-
 
         self.fail('Finish the test')
 
