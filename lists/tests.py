f97c4258f632c0257401b51dc6a2d4216b0a30d1
satellitestar <1752553357@qq.com>
2020-06-02 21:12:31 +0800

    第7个ppt中间51页，改完网页和view

--- lists/tests.py
+++ lists/tests.py
@@ -12,19 +12,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
 
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
-
-
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
         # self.assertIn('A new list item',response.content.decode())
         # self.assertTemplateUsed(response,'home.html')
@@ -43,9 +31,9 @@ class HomePageTest(TestCase):
 
 
 
-    def test_only_save_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
+    # def test_only_save_items_when_necessary(self):
+    #     self.client.get('/')
+    #     self.assertEqual(Item.objects.count(), 0)
 
 
 
@@ -86,3 +74,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
