472f366f83b01c2a55022598459a310714f8b4f6
satellitestar <1752553357@qq.com>
2020-06-02 20:56:52 +0800

    第7个ppt中间42页，分成了两个页面

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,51 +15,55 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    # def test_can_start_a_list_and_retrieve_it_later(self):
-    #     self.browser.get(self.live_server_url)
-    #
-    #     self.assertIn('To-Do',self.browser.title)
-    #
-    #     header_test = self.browser.find_element_by_tag_name('h1').text
-    #     self.assertIn('To-Do',header_test)
-    #
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertEqual(
-    #         inputbox.get_attribute('placeholder'),
-    #         'Enter a To-Do item'
-    #     )
-    #
-    #     inputbox.send_keys('Buy peacock feathers')
-    #
-    #     inputbox.send_keys(Keys.ENTER)
-    #     self.wait_for_row_in_list_table('1:Buy peacock feathers')
-    #
-    #     # table = self.browser.find_element_by_id('id_list_table')
-    #     # rows = table.find_elements_by_tag_name('tr')
-    #     # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-    #
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     inputbox.send_keys('Use peacock feathers to make a fly')
-    #     inputbox.send_keys(Keys.ENTER)
-    #
-    #     self.wait_for_row_in_list_table('1:Buy peacock feathers')
-    #     self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
-    #
-    #     # table = self.browser.find_element_by_id('id_list_table')
-    #     # rows = table.find_elements_by_tag_name('tr')
-    #     # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-    #     # self.assertIn(
-    #     #     '2:Use peacock feathers to make a fly',
-    #     #     [row.text for row in rows]
-    #     # )
-    #
-    #     self.fail('Finish the test')
     def test_can_start_a_list_for_one_user(self):
-        #Edith has heard a new coll online to-do app, she goses
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do',self.browser.title)
+
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_test)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a To-Do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         #the page update again, and now shows both items on her list
         self.wait_for_row_in_list_table('1:Buy peacock feathers')
         self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
-        #Satisfied she goes to sleep
+
+        # Satisfied she goes to sleep
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn(
+        #     '2:Use peacock feathers to make a fly',
+        #     [row.text for row in rows]
+        # )
+
+        # self.fail('Finish the test')
+
+    # def test_can_start_a_list_for_one_user(self):
+    #     #Edith has heard a new coll online to-do app, she goses
+    #     #the page update again, and now shows both items on her list
+    #     self.wait_for_row_in_list_table('1:Buy peacock feathers')
+    #     self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+    #     #Satisfied she goes to sleep
 
     def test_multiple_users_can_start_at_different_urls(self):
         #Edith start a new url
@@ -78,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
         ##We use a new browser session to make sure that no information
         ##of Edith's is comeing through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.firefox()
+        self.browser = webdriver.Firefox()
 
         #Francis visits the home page, there is no sign of edith's list
         self.browser.get(self.live_server_url)
