56fc9fa8103f34e13a0398416fa7d86c0261e91d
satellitestar <1752553357@qq.com>
2020-06-02 15:32:18 +0800

    第7个ppt，改完功能测后报错

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,52 +15,108 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    # def test_can_start_a_list_and_retrieve_it_later(self):
+    #     self.browser.get(self.live_server_url)
+    #
+    #     self.assertIn('To-Do',self.browser.title)
+    #
+    #     header_test = self.browser.find_element_by_tag_name('h1').text
+    #     self.assertIn('To-Do',header_test)
+    #
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a To-Do item'
+    #     )
+    #
+    #     inputbox.send_keys('Buy peacock feathers')
+    #
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1:Buy peacock feathers')
+    #
+    #     # table = self.browser.find_element_by_id('id_list_table')
+    #     # rows = table.find_elements_by_tag_name('tr')
+    #     # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+    #
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Use peacock feathers to make a fly')
+    #     inputbox.send_keys(Keys.ENTER)
+    #
+    #     self.wait_for_row_in_list_table('1:Buy peacock feathers')
+    #     self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+    #
+    #     # table = self.browser.find_element_by_id('id_list_table')
+    #     # rows = table.find_elements_by_tag_name('tr')
+    #     # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+    #     # self.assertIn(
+    #     #     '2:Use peacock feathers to make a fly',
+    #     #     [row.text for row in rows]
+    #     # )
+    #
+    #     self.fail('Finish the test')
+    def test_can_start_a_list_for_one_user(self):
+        #Edith has heard a new coll online to-do app, she goses
+        #the page update again, and now shows both items on her list
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+        self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        #Satisfied she goes to sleep
+
+    def test_multiple_users_can_start_at_different_urls(self):
+        #Edith start a new url
         self.browser.get(self.live_server_url)
-
-        self.assertIn('To-Do',self.browser.title)
-
-        header_test = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do',header_test)
-
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a To-Do item'
-        )
-
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        #She notice that her url is unique url
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
+        #Now a new user, Francis ,comes along to the site
+
+        ##We use a new browser session to make sure that no information
+        ##of Edith's is comeing through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.firefox()
+
+        #Francis visits the home page, there is no sign of edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #Francis start a new list by entering a new item, he is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1:Buy milk')
 
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
-        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        #Francis get his own unique url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        # self.assertIn(
-        #     '2:Use peacock feathers to make a fly',
-        #     [row.text for row in rows]
-        # )
+        #Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        self.fail('Finish the test')
+        #satisfied they both go sleep
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
 
 
